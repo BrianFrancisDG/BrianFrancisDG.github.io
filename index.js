10dc9331340b74abb50984b29c9976e3586c1f7c
Brian Francis De Guzman <deguzmanbrianfrancis@gmail.com>
2017-05-25 13:36:05 -0400

    Added Materialize Folders

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by briandeguzman on 4/25/17.
+ */
+
+$('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true,// Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true // Choose whether you can drag to open on touch screens
+
+    }
+  );
+
